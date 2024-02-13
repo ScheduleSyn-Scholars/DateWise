b304a6c32d1f2f69b6b1317cec4067d1d495084b
Jordan Moats <jmoats@ggc.edu>
2024-02-13 20:34:15 +0000

    adds grizzly daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,21 @@ module.exports = {
         },
     },
     plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                grizzly: {
+                    "primary": "#00704a",
+                    "secondary": "#3c9635",
+                    "accent": "#4af50c",
+                    "neutral": "#00704a",
+                    "base-100": "#f4f4f3",
+                    "info": "#0b7dbc",
+                    "success": "#00ff00",
+                    "warning": "orange",
+                    "error": "#ff0000",
+                },
+              }
+        ]
+    }
 };
