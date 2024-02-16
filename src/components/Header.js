6bc10a4ac081644127179474e97cfe829d9086d2
Jordan Moats <jmoats@ggc.edu>
2024-02-16 20:37:00 +0000

    Fix capitalization of logo filename

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="flex w-full items-center bg-green-800 px-2 py-2 text-white">
-            <img src="bearlogo.png" alt="Logo" className="h-28 w-auto" />
+            <img src="BearLogo.png" alt="Logo" className="h-28 w-auto" />
 
             <p className="font m1-auto text-5xl">DateWise</p>
 
