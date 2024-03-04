66f0a2227fba7dd76d2a8126c7a214a8ba856eb2
Jordan Moats <jmoats@ggc.edu>
2024-03-04 23:18:33 +0000

    adds notification bell to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { firestore } from '../resources/firebase';
 import { useUser } from '../resources/UserContext';
+import NotificationBell from './NotificationBell';
 
 const Header = () => {
     const [profilePictureUrl, setProfilePictureUrl] = useState(null);
@@ -19,6 +20,8 @@ const Header = () => {
                         const userData = userDoc.data();
                         if (userData && userData.imageURL) {
                             setProfilePictureUrl(userData.imageURL);
+                        } else {
+                            setProfilePictureUrl('Screenshot 2023-09-15 at 1.46 1.png');
                         }
                     }
                 }
@@ -37,15 +40,17 @@ const Header = () => {
             <p className="font m1-auto text-5xl">DateWise</p>
 
             <div className="flex-grow"></div>
+            <NotificationBell />
 
             <Link to="/MyProfile">
-                <div className="pr-4">
+                <div className="pr-4 w-24">
                     {profilePictureUrl ? (
-                        <img
-                            alt="User profile"
-                            src={profilePictureUrl}
-                            className="mt-2 h-24 w-24 rounded-full bg-gray-300"
-                        />
+                        <div className='flex items-center justify-center mt-2 h-24 w-24 rounded-full bg-gray-300'>
+                            <img
+                                alt="User profile"
+                                src={profilePictureUrl}
+                            />
+                        </div>
                     ) : (
                         <div className="h-24 w-24 rounded-full bg-gray-300"></div>
                     )}
