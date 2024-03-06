f24dd742634932628b018f7e01f955071fe66060
Jordan Moats <jmoats@ggc.edu>
2024-03-06 00:24:07 +0000

    fixes path typo displaying the header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
 
     return (
         <header className="flex w-full items-center bg-green-800 px-2 py-2 text-white">
-            <img src="BearLogo.png" alt="Logo.png" className="h-28 w-auto" />
+            <img src="/BearLogo.png" alt="Logo.png" className="h-28 w-auto" />
 
             <p className="font m1-auto text-5xl">DateWise</p>
 
