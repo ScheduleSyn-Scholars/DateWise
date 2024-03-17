4e517ce10e6e397a1f8233ae669b058d6b1f49e9
BrianMorga <bmorga@ggc.edu>
2024-03-17 14:11:58 -0400

    made header smaller

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,12 @@ const Header = () => {
 
     return (
         <header className="flex w-full items-center bg-green-800 px-2 py-2 text-white">
-            <img src="/BearLogo.png" alt="Logo.png" className="h-28 w-auto" />
+            <img src="/BearLogo.png" alt="Logo.png" className="h-20 w-auto" />
 
-            <p className="font m1-auto text-5xl">DateWise</p>
+            <p className="font m1-auto text-4xl">DateWise</p>
 
             <div className="flex-grow"></div>
-            <div class="flex items-center space-x-5">
+            <div class="flex items-center space-x-5 pr-5">
                 <NotificationBell />
                 <UserProfileModal isOpen={isProfileOpen} setIsOpen={setIsProfileOpen}/>
             </div>
